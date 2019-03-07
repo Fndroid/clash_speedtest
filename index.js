8e79d375bb5ef999051d77f8ffd1e764e8655835
Fndroid <18825176954@163.com>
2019-03-07 16:16:08 +0800

    update

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const speedTest = require('speedtest-net');
+// const speedTest = require('speedtest-net');
+const speedTest = require('./speedtest/index')
+require('draftlog').into(console)
 const axios = require('axios')
 const node_xlsx = require('node-xlsx')
 const fs = require('fs')
@@ -6,7 +8,7 @@ const path = require('path')
 const moment = require('moment')
 
 const EC = "127.0.0.1:9090" // 外部控制器 External Controller
-const TIME = 10 // 测试时间，单位秒
+const TIME = 7 // 测试时间，单位秒
 const PROXY = "http://127.0.0.1:7890" // Clash的http代理
 
 let xlsx = node_xlsx.default
@@ -61,18 +63,27 @@ let switchToNode = async (node) => {
   }
 }
 
+function ProgressBar(done, sum, min) {
+  let progress = done * 100 / sum
+  // Make it 50 characters length
+  var units = Math.round(progress / 2)
+  return '[' + '='.repeat(units) + ' '.repeat(50 - units) + '] ' + done + '/' + sum + '  ' + min + ' min' + (min > 1 ? 's':'')
+}
+
 async function main() {
   console.log(`测速开始...`)
   if (await setMode("Global")) {
-    console.log(`Clash成功切换至Global模式，请耐心等待测试完成...\n------------------------------------------------------\n`)
+    console.log(`Clash成功切换至Global模式，请耐心等待测试完成...\n------------------------------------------------------`)
     let nodeList = (await getNodeList())
     let resultArr = [['Proxy', 'Ping(ms)', 'Country', 'Upload(Mbps)', 'Download(Mbps)']]
+    var barLine = console.draft('开始测试...')
     for (var i = 0; i < nodeList.length; i++) {
       if (await switchToNode(nodeList[i])) {
         // process.env["HTTP_PROXY"] = process.env["http_proxy"] = PROXY
         let result = await startTesting(i)
-        let timeLeft = (nodeList.length - 1 - i) * TIME * 1000
-        console.log(`节点 ${nodeList[i]} 测试完成，剩余 ${nodeList.length - i - 1} 个待测节点，预计耗时 ${moment.duration(timeLeft).minutes() + 1} 分钟`)
+        let timeLeft = (nodeList.length - 1 - i) * (TIME + 10) * 1000
+        // console.log(`节点 ${nodeList[i]} 测试完成，剩余 ${nodeList.length - i - 1} 个待测节点，预计耗时 ${moment.duration(timeLeft).minutes() + 1} 分钟`)
+        barLine(ProgressBar(i + 1, nodeList.length, moment.duration(timeLeft).minutes() + 1))
         resultArr.push([nodeList[i], result.server.ping, result.server.country, result.speeds.upload, result.speeds.download])
         // process.env["HTTP_PROXY"] = process.env["http_proxy"] = ""
       } else {
@@ -86,11 +97,11 @@ async function main() {
     } catch (e) { }
     let fileName = moment().format("YYYY-MM-DD-HHmmss") + '.xlsx'
     fs.writeFileSync(path.join('output', fileName), xbuffer)
-    console.log(`\n------------------------------------------------------\n* 测试结果已写入 output 文件夹，文件名为：${fileName}\n`)
+    console.log(`------------------------------------------------------\n* 测试结果已写入 output 文件夹，文件名为：${fileName}\n`)
   } else {
     console.log(`Clash外部控制器无法连接，请将在此文件顶部EC（默认：127.0.0.1:9090）字段改为对应的值！`)
   }
-  console.log(`恢复为Rule模式${(await setMode("Rule")) ? '成功':'失败'}`)
+  console.log(`恢复为Rule模式${(await setMode("Rule")) ? '成功' : '失败'}`)
   console.log('测速完成...')
 }
 
